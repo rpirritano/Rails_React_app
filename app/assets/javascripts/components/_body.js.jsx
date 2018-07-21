134cc36bf6d1ccae3c0c858abc6e04f5bccb0cfe
Robert Pirritano <rpirritano@gmail.com>
2018-07-21 07:42:11 -0700

    completed action to add fruit

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -4,7 +4,30 @@ constructor(props) {
     this.state = {
       fruits: []
     };
+    this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    this.addNewFruit = this.addNewFruit.bind(this)
   }
+
+  handleFormSubmit(name, description){
+    let body = JSON.stringify({fruit: {name: name, description:   description} })
+    fetch('http://localhost:3000/api/v1/fruits', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: body,
+      }).then((response) => {return response.json()})
+      .then((fruit)=>{
+        this.addNewFruit(fruit)
+      })
+
+    }
+    addNewFruit(fruit){
+      this.setState({
+        fruits: this.state.fruits.concat(fruit)
+    })
+  }
+
 componentDidMount(){
     fetch('/api/v1/fruits.json')
       .then((response) => {return response.json()})
@@ -13,7 +36,7 @@ componentDidMount(){
 render(){
     return(
       <div>
-        <NewFruit />
+        <NewFruit handleFormSubmit={this.handleFormSubmit} />
         <AllFruits fruits={this.state.fruits} />
       </div>
     )
--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -5,7 +5,7 @@ const NewFruit = (props) => {
     <div>
      <input ref={input => formFields.name = input} placeholder='Enter the name of the item'/>
      <input ref={input => formFields.description = input} placeholder='Enter a description' />
-     <button>Submit</button>
+     <button onClick={ () => props.handleFormSubmit(formFields.name.value, formFields.description.value)}>Submit</button>
       </div>
     )
 }
