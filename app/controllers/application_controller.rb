4f8e635655476302f0f0804d8808e82300c00c90
Robert Pirritano <rpirritano@gmail.com>
2018-07-21 06:55:58 -0700

    added fruits controller with action and config routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+=begin
+  null_session should be used in API-style controllers, where you have no use for the session object.
+  The user's preexisting session will remain intact.
+=end
 end
